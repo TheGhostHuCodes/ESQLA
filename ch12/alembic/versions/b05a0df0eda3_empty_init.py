c55742dcf0259c11333f5e2229705387a6f5a634
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2020-12-22 18:21:23 -0800

    Add alembic migration setup and initial migration

---
+++ ch12/alembic/versions/b05a0df0eda3_empty_init.py
@@ -0,0 +1,23 @@
+"""Empty init
+
+Revision ID: b05a0df0eda3
+Revises:
+Create Date: 2020-12-22 17:53:45.922932
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "b05a0df0eda3"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
